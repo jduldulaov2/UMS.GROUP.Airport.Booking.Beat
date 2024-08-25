378e25ce41d9a7b819e8865279b557503baf9715
Jake Duldulao <Jake.Duldulao@dbresults.com.au>
2024-08-25 22:08:57 +0800

    commit

--- src/Web/ClientApp/src/app/Shared/header-client/header-client.component.ts
+++ src/Web/ClientApp/src/app/Shared/header-client/header-client.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthClient } from '../../web-api-client';
 
 @Component({
   selector: 'app-header-client',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-client.component.css']
 })
 export class HeaderClientComponent {
+  isExpanded = false;
+  IsLoggedIn: any;
+
+  local_url!: any;
+
+  constructor(private authClient: AuthClient) {}
+
+  ngOnInit(){
+    this.GetLogin();
+  }
+
+  GetLogin(){
+    // READ STRING FROM LOCAL STORAGE
+  var retrievedObject = localStorage.getItem('loggedindetail');
+
+  if (typeof retrievedObject !== 'undefined' && retrievedObject !== null){
+    // CONVERT STRING TO REGULAR JS OBJECT
+    var parsedObject = JSON.parse(retrievedObject!);
+    
+    this.IsLoggedIn = true;
+  }else{
+    this.IsLoggedIn = false;
+  }
+  }
+
+  SignOut(){
+    this.authClient.geLoggedIn().subscribe({
+      next: result => {
+        if(result.resultType == 1){
+          var loggedInId = result.data?.loggedInId;
+          this.ProceedSignOut(loggedInId);
+        }else{
+          location.href = '/login';
+        }
+      },
+      error: error => console.error(error)
+    });
+  }
+
+  ProceedSignOut(id: any){
+    this.authClient.logOut(id).subscribe({
+      next: result => {
+        if(result.resultType == 1){
+          localStorage.clear();
+          location.href = '/login';
+        }
+      },
+      error: error => console.error(error)
+    });
+  }
 
 }
--- src/Web/ClientApp/src/app/Shared/sidebar-client/sidebar-client.component.ts
+++ src/Web/ClientApp/src/app/Shared/sidebar-client/sidebar-client.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthClient } from '../../web-api-client';
 
 @Component({
   selector: 'app-sidebar-client',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar-client.component.css']
 })
 export class SidebarClientComponent {
+
+  isExpanded = false;
+  IsLoggedIn: any;
+
+  local_url!: any;
+
+  constructor(private authClient: AuthClient) {}
+
+  ngOnInit(){
+    this.GetLogin();
+  }
+
+  GetLogin(){
+    // READ STRING FROM LOCAL STORAGE
+  var retrievedObject = localStorage.getItem('loggedindetail');
+
+  if (typeof retrievedObject !== 'undefined' && retrievedObject !== null){
+    // CONVERT STRING TO REGULAR JS OBJECT
+    var parsedObject = JSON.parse(retrievedObject!);
+    
+    this.IsLoggedIn = true;
+  }else{
+    this.IsLoggedIn = false;
+  }
+  }
+
+  SignOut(){
+    this.authClient.geLoggedIn().subscribe({
+      next: result => {
+        if(result.resultType == 1){
+          var loggedInId = result.data?.loggedInId;
+          this.ProceedSignOut(loggedInId);
+        }else{
+          location.href = '/login';
+        }
+      },
+      error: error => console.error(error)
+    });
+  }
+
+  ProceedSignOut(id: any){
+    this.authClient.logOut(id).subscribe({
+      next: result => {
+        if(result.resultType == 1){
+          localStorage.clear();
+          location.href = '/login';
+        }
+      },
+      error: error => console.error(error)
+    });
+  }
 
 }
