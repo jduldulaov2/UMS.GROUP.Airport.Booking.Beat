48de5e7f3c697a2fc8cdb8768e0b686681888e10
Jake Duldulao <Jake.Duldulao@dbresults.com.au>
2024-08-29 22:53:54 +0800

    commit

--- src/Web/ClientApp/src/app/Pages/AdminPortal/manage-customer/manage-customer.component.ts
+++ src/Web/ClientApp/src/app/Pages/AdminPortal/manage-customer/manage-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UsersClient, UsersListDto, UsersDto } from '../../../web-api-client';
+import { ActivatedRoute, Router } from '@angular/router';
+declare var $: any;
 
 @Component({
   selector: 'app-manage-customer',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-customer.component.css']
 })
 export class ManageCustomerComponent {
+
+  
+  public usersDto: UsersDto[] = [];
+  public usersListDto: UsersListDto;
+
+  constructor(
+    private usersClient: UsersClient
+  ) {
+  }
+
+  ngOnInit(){
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    this.getUserListListByName('','1','1000');
+  }
+
+  getUserListListByName(name: any, page1: any, page2:any): void {
+    this.usersClient.allUsersByName(name, page1, page2).subscribe({
+      next: result => {
+        this.usersDto = result.data?.users!;
+        console.log(this.usersDto);
+      },
+      error: error => console.error(error)
+    });
+  }
 
 }
